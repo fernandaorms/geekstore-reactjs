0b23b36c2b18de29429496f4c3c746fdfd9ae77b
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-05-07 09:27:02 -0300

    add Clients

---
+++ src/utils/tables/table-clients-columns.js
@@ -0,0 +1,22 @@
+export const columns = [
+    {
+        'api': 'id',
+        'value': 'ID'
+    },
+    {
+        'api': 'first_name',
+        'value': 'Nome'
+    },
+    {
+        'api': 'last_name',
+        'value': 'Sobrenome'
+    },
+    {
+        'api': 'email',
+        'value': 'Email'
+    },
+    {
+        'api': 'phone',
+        'value': 'Telefone'
+    }
+];
---
+++ src/views/private/Clients.js
@@ -0,0 +1,26 @@
+import { Table } from '../../components/common/Table';
+import { columns } from '../../utils/tables/table-clients-columns';
+
+export const Clients = () => {
+    const tableConfig = {
+        tableId: 'clientList',
+        apiUrl: '/client',
+        errorMessage: 'Não foi possível carregar a lista de Clientes, consulte os desenvolvedores.',
+        emptyTableMessage: 'Não há Clientes cadastrados.',
+        columns: columns
+    };
+
+    return (
+        <div className='main'>
+            <div className='introduction'>
+                <div className='title'>
+                    <h1>Clientes</h1>
+                </div>
+            </div>
+            
+            <div>
+                <Table tableConfig={tableConfig} />
+            </div>
+        </div>
+    );
+};
